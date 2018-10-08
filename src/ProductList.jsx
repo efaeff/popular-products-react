e27e28fbc3d0a0614a51852e42921533ad952e69
efaeff <ewasiola9@gmail.com>
2018-10-08 12:17:26 +0200

    Add Basic Style

---
+++ src/ProductList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export class ProductList extends React.Component {
+  render() {
+    return (
+      <header className="header">
+        <p>Popular Products</p>
+      </header>
+    );
+  }
+}
