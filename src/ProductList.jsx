5760a42d1ea4782c024d1e69b7f654a903ddbd71
efaeff <ewasiola9@gmail.com>
2018-10-12 12:31:23 +0200

    Adding cats

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -4,19 +4,18 @@ import { products } from './seed';
 
 export class ProductList extends React.Component {
   render() {
-    const product = products[0];
-    return (
-      <div className="ui unstackable items">
-        <Product
-          id={product.id}
-          title={product.title}
-          description={product.description}
-          url={product.url}
-          votes={product.votes}
-          submitterAvatarUrl={product.submitterAvatarUrl}
-          productImageUrl={product.productImageUrl}
-        />
-      </div>
-    );
+    const productComponents = products.map(product => (
+      <Product
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitterAvatarUrl}
+        productImageUrl={product.productImageUrl}
+      />
+    ));
+
+    return <div className="ui unstackable items">{productComponents}</div>;
   }
 }
--- src/seed.js
+++ src/seed.js
@@ -3,13 +3,46 @@ const generateVoteCount = () => 'votes';
 export const products = [
   {
     id: 1,
-    title: 'Yellow Pail',
-    description: 'On-demand sand castle construction expertise',
+    title: 'Cute babies',
+    description: 'Sooooooooooo Cute Vote For Us<3',
     url: '#',
     votes: generateVoteCount(),
     submitterAvatarUrl:
       'https://images.pexels.com/photos/259803/pexels-photo-259803.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
     productImageUrl:
       'https://images.pexels.com/photos/4602/jumping-cute-playing-animals.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  },
+  {
+    id: 2,
+    title: 'Very Cute',
+    description: 'I Am So Lovely, Please Vote For Me<3',
+    url: '#',
+    votes: generateVoteCount(),
+    submitterAvatarUrl:
+      'https://images.pexels.com/photos/615369/pexels-photo-615369.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    productImageUrl:
+      'https://images.pexels.com/photos/674570/pexels-photo-674570.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  },
+  {
+    id: 3,
+    title: 'Cute Blue Eyes',
+    description: 'I Am Cute Vote For Me<3',
+    url: '#',
+    votes: generateVoteCount(),
+    submitterAvatarUrl:
+      'https://images.pexels.com/photos/225406/pexels-photo-225406.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    productImageUrl:
+      'https://images.pexels.com/photos/357141/pexels-photo-357141.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+  },
+  {
+    id: 4,
+    title: 'Very Cute Black Cat',
+    description: 'Wow wow Vote For Me<3',
+    url: '#',
+    votes: generateVoteCount(),
+    submitterAvatarUrl:
+      'https://images.pexels.com/photos/37337/cat-silhouette-cats-silhouette-cat-s-eyes.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    productImageUrl:
+      'https://images.pexels.com/photos/302280/pexels-photo-302280.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
   },
 ];
